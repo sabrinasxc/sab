b5e9378dbcc905d6f065b073680d383422a1f7a9
sabrinasxc <sabrina@sabrinarunbeck.com>
2026-09-06 22:18:12 -0500

    Add Supabase server client

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,25 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { getServerEnv } from "@/lib/env";
+
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
+  const env = getServerEnv();
+
+  return createServerClient(
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          for (const { name, value, options } of cookiesToSet) {
+            cookieStore.set(name, value, options);
+          }
+        },
+      },
+    },
+  );
+}
